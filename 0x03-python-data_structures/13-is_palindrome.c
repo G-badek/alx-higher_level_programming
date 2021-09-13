4aaf3345a89c12ea1fdc48d6cab337fb7af71b52
Fortunate <akinjibadefortunate@gmail.com>
2021-09-13 13:18:51 +0000

    Is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,13 +3,33 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
-int main(void)
+ * palindrome - check palindrome in sigly linked list
+ * @left: init singly linked list
+ * @right: end singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ **/
+int palindrome(listint_t **left, listint_t *right)
 {
-listint_t *head;
-return (0);
+	int isPalindrome;
+	if (right == NULL)
+	{
+		return (1);
+	}
+	isPalindrome = (palindrome(left, right->next) && (*left)->n == right->n);
+	*left = (*left)->next;
+	return (isPalindrome);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!(*head) || !head)
+	{
+		return (1);
+	}
+	return (palindrome(head, *head));
 }
