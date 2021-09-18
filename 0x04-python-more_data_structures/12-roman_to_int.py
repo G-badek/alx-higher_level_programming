4a69fef1d6dea4ebc539ce60e00e40be134d1a9f
Fortunate <akinjibadefortunate@gmail.com>
2021-09-18 01:55:21 +0000

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
+    
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = maximun = 0
     for num in range(len(roman_string) - 1, -1, -1):
@@ -12,4 +13,4 @@ def roman_to_int(roman_string):
 
         maximun = roman[roman_string[num]]
 
-        return number
+    return number
